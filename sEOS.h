7256a26d4c27d4da9dfae46056767805b50a2f33
Franciskoxz <55772354+Franciskoxz@users.noreply.github.com>
2022-05-13 18:14:40 -0300

    Interrupciones

---
+++ sEOS.h
@@ -0,0 +1,17 @@
+/*
+ * sEOS.h
+ *
+ */ 
+
+
+#ifndef SEOS_H_
+#define SEOS_H_
+
+
+void SEOS_Init(void);
+void SEOS_Schedule_Tasks(void);
+void SEOS_Dispatch_Tasks (void);
+
+
+
+#endif /* SEOS_H_ */
