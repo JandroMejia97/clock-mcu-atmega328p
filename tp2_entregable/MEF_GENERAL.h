2e81c7e11e0b49792e7cd5e47bca9d69b28676cd
Carlos Alejandro Hernández Mejía <alejandromejia2013.27@gmail.com>
2022-05-14 16:55:19 -0300

    Initialize project

--- tp2_entregable/MEF_GENERAL.h
+++ tp2_entregable/MEF_GENERAL.h
@@ -11,7 +11,7 @@ void MEF_GENERAL_Init(void);
 void MEF_GENERAL_Update(void);
 //void MEF_GENERAL_ResetCounter(void);
 
-typedef enum {DEFAULT, MODIFICAR_DATO, MOD_DIA, MOD_MES, MOD_ANIO, MOD_HORA, MOD_MINUTOS, MOD_SEGUNDOS} MEF_state;
+typedef enum { DEFAULT, EDITION, EDITION_ENDED, CANCELED } MEF_state;
 
 
 #endif /* MEF_GENERAL_H_ */
