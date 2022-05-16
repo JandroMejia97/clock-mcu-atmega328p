db218a77269637705daf7eba20dd1bd0fc32cd93
Franciskoxz <fran_mayo_boca@hotmail.com>
2022-05-15 23:32:05 -0300

    Keypad de facu

--- tp2_entregable/keypad.c
+++ tp2_entregable/keypad.c
@@ -14,28 +14,36 @@ void KEYPAD_Init(void) {
 }
 
 
-const uint8_t keys[4] = {'A','B','C','D'};
+const uint8_t keys[4][4] = {
+	{'1','2','3','A'},
+	{'4','5','6','B'},
+	{'7','8','9','C'},
+	{'*','0','#','D'},
+};
 
 
-uint8_t KEYPAD_Scan(uint8_t *pressed_key) {
+uint8_t KEYPAD_Scan(uint8_t *pressed_key) 
+{
 	uint8_t mask[] = {0b11111110, 0b11111101, 0b11111011, 0b11110111};
 	PORTB = 0xF0;
-	uint8_t i;
+	uint8_t i, j;
 	for (i = 0; i<4; i++) {	//se barren las filas
 		PORTB |= mask[i];
-		if ((~PINB) & (1 << (i+4))) {
-			*pressed_key = keys[i];
-			PORTB |= 0xF0;
-			return 1;
+		for (j = 0; j<4; j++) {	//se barren las columnas
+			if ((~PINB) & (1 << (j+4))) {
+				*pressed_key = keys[i][j];
+				PORTB |= 0xF0;
+				return 1;
+			}
 		}
+		PORTB = 0xF0;
 	}
-	PORTB = 0xF0;
 	return 0;
 }
 
 /************************************************************************/
-/* Retorna 1 si una tecla fue presionada y la devuelve por parï¿½metro    */
-/* evitando detecciï¿½n multiple y efecto rebote.						    */
+/* Retorna 1 si una tecla fue presionada y la devuelve por parámetro    */
+/* evitando detección multiple y efecto rebote.						    */
 /************************************************************************/
 uint8_t KEYPAD_Update(uint8_t *pressed_key)
 {
