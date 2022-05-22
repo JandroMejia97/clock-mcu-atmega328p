e676ed8ec41b901c80ac0eee3c022b00631e0f9c
Carlos Alejandro Hernández Mejía <alejandromejia2013.27@gmail.com>
2022-05-22 20:20:00 -0300

    Keyboard update

--- tp2_entregable/keypad.c
+++ tp2_entregable/keypad.c
@@ -5,120 +5,93 @@
 
 #include <avr/io.h>
 #include "keypad.h"
-#define F_CPU 16000000L
+#define F_CPU 16000000UL
 #include <util/delay.h>
 
-//pines para las filas, de A a D
-const uint8_t row[4]={4,3,0,7};
-//pines para las columnas, de 1 a 4
-const uint8_t col[4]={3,5,4,2};
-//valores ASCII de cada tecla
-const char ascii[16]= {	'1','2','3','A',
-	'4','5','6','B',
-	'7','8','9','C',
-	'0','*','#','D' };
+static const uint8_t keys[4][4] = {
+	{'1','2','3','A'},
+	{'4','5','6','B'},
+	{'7','8','9','C'},
+	{'*','0','#','D'},
+};
 
-/************************************************************************
-Esta funcion configura los puertos correspondientes a las filas como
-salida, y los pines correspondientes a las columnas como entrada con pull up.
+// B4, B3, B0 y D7 como filas
+static const uint8_t pin_rows[4] = {4, 3, 0, 7};
 
-Inicializa las salidas de cada fila como desactivada (1)
-***********************************************************************/
-void KEYPAD_Init(){
-	//inicializo puertos de filas como salida, inicialmente inactivas (salida 1)
-	for(int i=0;i<3;i++){
-		DDRB|=1<<row[i];
-		PORTB|=1<<row[i];
-		
+// D3, D5, D4 y D2 como columnas
+static const uint8_t pin_columns[4] = {3, 5, 4, 2};
+
+void KEYPAD_Setup() {
+	for (uint8_t i = 0; i < 3; i++) {
+		PORTB |= (1 << pin_rows[i]);
+		PORTD |= (1 << pin_columns[i]);
 	}
-	DDRD|=1<<row[3];
-	PORTD|=1<<row[3];
-	
-	//inicializo pines de columnas como entrada con pull up
-	for(int i=0;i<4;i++){
-		DDRD&=~(1<<col[i]);
-		PORTD|=1<<col[i];
-	}
+	PORTD |= 1 << pin_rows[3];
+	PORTD |= 1 << pin_columns[3];
 }
-/****************************************************************
-La funcion keypad_scan realiza polling fila por fila en el teclado
-matricial, devolviendo 1 si detecta una tecla pulsada o 0 caso contrario.
-Este proceso lo llamamos "escaneo".
 
-Argumentos:
-
-*pkey: referencia a la variable donde se almacenara la tecla que se detecte pulsada
-
-Nota: debido al funcionamiento del teclado, solo es posible detectar una tecla
-por cada llamado a la funcion. Si hay mas de una tecla presionada
-se devuelve la que se encuentra primero teniendo en cuenta
-el orden de escaneo (fila por fila, de izquierda a derecha)
-****************************************************************/
-uint8_t KEYPAD_Scan(uint8_t *pkey) {
-	uint8_t i;
-	uint8_t j;
-	
-	for(i=0;i<3;i++){
-		//activo la fila i para el polling
-		PORTB&=~(1<<row[i]);
-		for(j=0;j<4;j++){
-			//busco columna activa en bajo
-			if (!( PIND & (1 << col[j]) )){
-				//desactivo fila i
-				PORTB|=1<<row[i];
-				*pkey=ascii[i*4 + j];
-				return 1;
-			}	
+void KEYPAD_Init(void) {
+	for (uint8_t i = 0; i < 4; i++) {
+		if (i < 3) {
+			DDRB &= ~(1 << pin_rows[i]);
+			PORTB |= (1 << pin_rows[i]);
 		}
-		//desactivo la fila i
-		PORTB|=1<<row[i];
+		DDRD |= (1 << pin_columns[i]);
+		PORTD |= (1 << pin_columns[i]);
 	}
-	//activo la ultima fila
-	PORTD&=~(1<<7);
-	for(j=0;j<4;j++){
-		//busco columna activa en bajo
-		if (!( PIND & (1 << col[j]) )){
-			PORTD|=1<<7;
-			//el primer boton de la fila 4 es el indice 12 del arreglo
-			*pkey=ascii[12 + j];
-			return 1;
+	DDRD &= ~(1<<pin_rows[3]);
+	PORTD |= (1<<pin_rows[3]);
+}
+
+uint8_t KEYPAD_Scan(uint8_t *pressed_key) 
+{
+	KEYPAD_Setup();	
+
+	uint8_t i, j;
+	for (i = 0; i < 4; i++) {	// se barren las columnas
+		PORTD &= ~(1 << pin_columns[i]);
+		for (j = 0; j < 4; j++) {	//se barren las filas
+			if (j < 3) {
+				if (!(PINB & (1 << pin_rows[j]))) {
+					*pressed_key = keys[j][i];
+					KEYPAD_Setup();
+					return 1;
+				}
+			} else {
+				if (!(PIND & (1 << pin_rows[j]))) {
+					*pressed_key = keys[j][i];
+					KEYPAD_Setup();
+					return 1;
+				}
+			}
 		}
+		KEYPAD_Setup();
 	}
-	//desactivo ultima fila
-	PORTD|=(1<<7);
 	return 0;
 }
+
 /************************************************************************/
-/* La funcion KEYPAD_update evita el efecto rebote de manera no bloqueante, descartando
-pulsaciones dobles consecutivas de la misma tecla*/
+/* Retorna 1 si una tecla fue presionada y la devuelve por par�metro    */
+/* evitando detecci�n multiple y efecto rebote.						    */
 /************************************************************************/
-uint8_t KEYPAD_Update(uint8_t *pkey)
+uint8_t KEYPAD_Update(uint8_t *pressed_key)
 {
-	static uint8_t Old_key;
-	uint8_t Key;
-	static uint8_t Last_valid_key = 0xFF;
-	
-	//lee el teclado
-	if (!KEYPAD_Scan(&Key))
-	{
-		//si no se recibe una tecla se resetean los valores y se devuelve 0
-		Old_key = 0xFF;
-		Last_valid_key = 0xFF;
+	static uint8_t old_key;
+	uint8_t key;
+	static uint8_t last_valid_key = 0xFF;
+
+	if (!KEYPAD_Scan(&key)) {
+		old_key = 0xFF;
+		last_valid_key = 0xFF;
 		return 0;
 	}
-	
-	//Si la ultima tecla leida sigue presionada. Esto permite que se estabilice la se�al del teclado.
-	if (Key == Old_key)
-	{
-		//verifica que no sea una pulsacion consecutiva y la valida
-		if (Key != Last_valid_key) {
-			*pkey = Key;
-			Last_valid_key = Key;
+	if (key == old_key) {
+		if (key != last_valid_key) {
+			*pressed_key = key;
+			last_valid_key = key;
 			return 1;
 		}
-	}
-	
-	//Se detecta la primera pulsacion de la tecla
-	Old_key = Key;
+	}	
+	old_key = key;
 	return 0;
 }
