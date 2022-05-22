b799ce591c7c67a39f2b33758b19c9ca5750fbdf
Carlos Alejandro Hernández Mejía <alejandromejia2013.27@gmail.com>
2022-05-22 19:09:01 -0300

    Reading the keypad

--- tp2_entregable/keypad.c
+++ tp2_entregable/keypad.c
@@ -8,17 +8,17 @@
 #define F_CPU 16000000UL
 #include <util/delay.h>
 
-const uint8_t port_masks[] = {0b00111100, 0b00000000};
+const uint8_t port_d_and_mask = 0b01111111;
+const uint8_t port_d_or_mask = 0b00111100;
+const uint8_t port_b_and_mask = 0b11100110;
 
 void KEYPAD_Init(void) {
-	 // DDRB = 0x0F;   // D7..D4 como entrada, D3..D0 como salida.
-	 DDRD = DDRD & 0b00111100;
-	 DDRB = 0b00000000;
-	 PORTD = PORTD | 0b10111100;
-	 PORTB = PORTB & 0b11100110;
-	 // PORTB = 0x0F;	// Habilito pull-ups internos de las entradas
+	 // D5..D2 como entrada, D7 como salida. Se mantiene la configuraci√≥n en el resto de pines
+	 DDRD = (DDRD & port_d_and_mask) | port_d_or_mask;
+	 // B4, B3 y B0 como entrada. Se mantiene la configuraci√≥n en el resto de pines
+	 DDRB = DDRB & port_b_and_mask;
+	 KEYPAD_Setup();
 }
-
 
 const uint8_t keys[4][4] = {
 	{'1','2','3','A'},
@@ -30,28 +30,53 @@ const uint8_t keys[4][4] = {
 
 uint8_t KEYPAD_Scan(uint8_t *pressed_key) 
 {
-	uint8_t mask[] = {0b11111110, 0b11111101, 0b11111011, 0b11110111};
-	uint8_t n_mask[] = {0b11101111, 0b11110111, 0b11111110, 0b01110000};
-	
-	PORTB = 0b00011001;
+	uint8_t rows_masks[] = {
+		0b00010000, // Fila 1 (B4) como entrada
+		0b00001000, // Fila 2 (B3) como entrada
+		0b00000001, // Fila 3 (B0) como entrada
+		0b10000000, // Fila 4 (D7) como entrada
+	};
+
+	uint8_t columns_masks[] = {
+		0b11110111, // Columna 1 (D3) como entrada
+		0b11011111, // Columna 2 (D5) como entrada
+		0b11101111, // Columna 3 (D4) como entrada
+		0b11111011 // Columna 4 (D2) como entrada
+	}; 
+
+	KEYPAD_Setup();	
+
 	uint8_t i, j;
-	for (i = 0; i<4; i++) {	//se barren las filas
-		PORTB |= mask[i];
-		for (j = 0; j<4; j++) {	//se barren las columnas
-			if ((~PINB) & (1 << (j+4))) {
-				*pressed_key = keys[i][j];
-				PORTB |= 0xF0;
-				return 1;
+	for (i = 0; i < 4; i++) {	// se barren las columnas
+		PORTD = PORTD & columns_masks[i];
+		for (j = 0; j < 4; j++) {	//se barren las filas
+			if (j < 3) {
+				if ((~PINB) & rows_masks[j]) {
+					*pressed_key = keys[i][j];
+					KEYPAD_Setup();
+					return 1;
+				}
+			} else {
+				if ((~PIND) & rows_masks[j]) {
+					*pressed_key = keys[j][i];
+					KEYPAD_Setup();
+					return 1;
+				}
 			}
 		}
-		PORTB = 0xF0;
+		KEYPAD_Setup();
 	}
 	return 0;
 }
 
+void KEYPAD_Setup() {
+	PORTD = (PORTD & port_d_and_mask) | port_d_or_mask; // Pongo en 0 el bit D7 y 1 los bits D5..D2 
+	PORTB = PORTB & port_b_and_mask; // Pongo en 0 los bits B4, B3 y B0
+}
+
 /************************************************************************/
-/* Retorna 1 si una tecla fue presionada y la devuelve por par·metro    */
-/* evitando detecciÛn multiple y efecto rebote.						    */
+/* Retorna 1 si una tecla fue presionada y la devuelve por parÔøΩmetro    */
+/* evitando detecciÔøΩn multiple y efecto rebote.						    */
 /************************************************************************/
 uint8_t KEYPAD_Update(uint8_t *pressed_key)
 {
--- tp2_entregable/keypad.h
+++ tp2_entregable/keypad.h
@@ -8,6 +8,7 @@
 #define KEYPAD_H_
 
 void KEYPAD_Init(void);
+void KEYPAD_Setup(void);
 uint8_t KEYPAD_Scan(uint8_t*);
 uint8_t KEYPAD_Update(uint8_t*);
 
